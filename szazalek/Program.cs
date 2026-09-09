d0d74ffd3a91b2e8db7039a1e527b4ddfc08e8aa
RomischerChristoferEmanuel <romischer.christoferemanuel.2024d@telekimezotur.hu>
2026-09-09 12:25:57 +0200

    adatbekérés

--- szazalek/Program.cs
+++ szazalek/Program.cs
@@ -1,1 +1,5 @@
-﻿Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Kérem a tanuló nevét:");
+string TanuloNeve = Console.ReadLine();
+Console.WriteLine("Kérem a tanuló pontszámát:");
+int TanuloPontszama = int.Parse(Console.ReadLine());
+int maxPontszam = 97;
